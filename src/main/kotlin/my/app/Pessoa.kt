168e81c3ebc49ae3d838139f71533106d7a17347
Fernando Galvão <fernando.galvao77@outlook.com>
2021-03-06 09:27:23 -0300

    Utilizando a função inner class - uma função interna de classe dentro de uma classe principal

--- src/main/kotlin/my/app/Pessoa.kt
+++ src/main/kotlin/my/app/Pessoa.kt
@@ -3,10 +3,17 @@ package my.app
 class Pessoa {
     var nome:String = "Fernando"
     var cpf:String = "123.456.789-xx"
+
+    inner class Endereco {
+        var rua:String = "Rua 28"
+    }
 }
 
 fun main() {
     val fernando = Pessoa()
+
     println(fernando.nome)
     println(fernando.cpf)
+
+    println(fernando.Endereco().rua)
 }
--- src/main/kotlin/my/app/teste.kt
+++ src/main/kotlin/my/app/teste.kt
@@ -1,14 +1,16 @@
 package my.app
 
+//meu primeiro código em Kotlin
+
 val firstName:String = "Fernando"
 val lastName:String  ="Galvão"
 val bornYear:Int = 1977
 var atualYear:Int = 2021
-var idade:Int = (atualYear - bornYear)
+var age:Int = (atualYear - bornYear)
 var occupation:String = "Student of Data Science"
 
 fun main() {
     println("Seja bem-vindo ao KOTLIN S2!")
     println("Espero que sua experiência seja excelente ;) \n")
-    println("\tNome: $firstName $lastName \n\tIdade: $idade \n\tOcupação: $occupation")
+    println("\tNome: $firstName $lastName \n\tIdade: $age \n\tOcupação: $occupation")
 }
