b7963bdb7a1bac8b8b0d11706152e2609f565fe5
Fernando Galvão <fernando.galvao77@outlook.com>
2021-03-06 10:34:40 -0300

    Criando classe primária conta

---
+++ src/main/kotlin/my/app/Conta.kt
@@ -0,0 +1,20 @@
+package my.app
+
+import java.math.BigDecimal
+import java.util.*
+
+class Conta (
+    val agencia:String,
+    val numeroConta:String,
+    val saldo:BigDecimal
+
+) {
+    fun deposito(valor:BigDecimal) {
+
+    }
+
+    fun saque(valor:BigDecimal) {
+
+    }
+
+}
