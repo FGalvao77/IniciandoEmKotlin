f21c445092b602c2d2bcc9db4135ad046095f999
Fernando Galvão <fernando.galvao77@outlook.com>
2021-03-06 09:59:28 -0300

    Criando o constructor e seus membros da classe

--- src/main/kotlin/my/app/Pessoa.kt
+++ src/main/kotlin/my/app/Pessoa.kt
@@ -2,15 +2,18 @@ package my.app
 
 class Pessoa {
     var nome:String = "Fernando"
-
     var cpf:String = "123.456.789-xx"
     private set
+
+    constructor()
+    fun pessoaInfo() = "\tNome: $nome \n\tCPF: $cpf"
 
 }
 
 fun main() {
     val fernando = Pessoa()
 
-    println(fernando.nome)
-    println(fernando.cpf)
+    println(fernando.pessoaInfo())
+    //println(fernando.nome)
+    //println(fernando.cpf)
 }
