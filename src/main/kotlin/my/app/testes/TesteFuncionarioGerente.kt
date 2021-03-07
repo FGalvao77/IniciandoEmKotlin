f56814229d1d0bc329abcd26b69236b6d3818ecc
Fernando Galvão <fernando.galvao77@outlook.com>
2021-03-07 15:35:53 -0300

    renomeando teste para testeMeuPrimeiroCodigo e formatando TesteFuncionarioGerente

--- src/main/kotlin/my/app/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/my/app/testes/TesteFuncionarioGerente.kt
@@ -7,13 +7,15 @@ fun main() {
 //    println("\tNome: ${fernando.nome}" +
 //            "\n\tCPF: ${fernando.cpf}\n")
 
+//    val katia = Pessoa(nome = "Kátia Galvão", cpf = "123.456.789-12")
+//    println("\tNome: ${katia.nome}" +
+//            "\n\tCPF: ${katia.cpf}\n")
 
     val fernando = Gerente(nome = "Fernando Galvão",
                            cpf = "147.258.369-14",
                            salario = 3780.00,
                            senha = "senha123")
-//    println("\tNome: ${katia.nome}" +
-//            "\n\tCPF: ${katia.cpf}")
+
     imprimeRelatorioFuncionario.imprime(fernando)
 
     TesteAutenticacao().autentica(fernando)
